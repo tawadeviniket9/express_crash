197b1d84b281bd8ffec0a148554ce9cc9f804085
Viniket9 <61202981+Viniket9@users.noreply.github.com>
2020-06-01 22:07:51 +0530

    Initial commit

---
+++ Members.js
@@ -0,0 +1,20 @@
+const members = [{
+    id:1,
+    name: 'John doe',
+    email: 'gmail',
+    status:'active'
+},
+{
+    id:2,
+    name: 'Bobby williams',
+    email: 'msn',
+    status:'active'
+    }
+,{
+    id:3,
+    name: 'Shananon doe',
+    email: 'outlook',
+    status:'active'
+}];
+
+module.exports = members;
